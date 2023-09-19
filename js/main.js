5a1f109747e52c75843248f7fdc8ff9351d613a7
Irene García <irenegwodak@gmail.com>
2023-09-19 09:26:38 +0200

    make basic js -> add constants+event+random function & change som html classes

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
 "use strict";
 console.log("ikuyo! :)")
+
+// DECLARATIONS
+
+const inputSelect = document.querySelector('.js-js-select');
+const btn = document.querySelector('.js-button');
+const printElement = document.querySelector('.js-print-result');
+
+// GENERIC FUNCTIONS
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+    }
+
+//SPECIFIC FUNCTIONS
+function handleClick() {
+}
+
+
+
+
+//EVENTS
+btn.addEventListener('click', handleClick);
