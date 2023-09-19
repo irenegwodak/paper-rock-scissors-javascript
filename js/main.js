d3900b8e61df178a2d35eeb622e69bcedf2e3f33
Irene García <irenegwodak@gmail.com>
2023-09-19 09:38:47 +0200

    make machine select

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-"use strict";
-console.log("ikuyo! :)")
+'use strict';
+console.log('ikuyo! :)');
 
 // DECLARATIONS
 
@@ -9,15 +9,30 @@ const printElement = document.querySelector('.js-print-result');
 
 // GENERIC FUNCTIONS
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-    }
+  return Math.ceil(Math.random() * max);
+}
 
 //SPECIFIC FUNCTIONS
 function handleClick() {
+  //   const userSelect = inputSelect.value;
+
+  /* machine: rock:0-3, paper:7+, else: scissors*/
+  const randomNumber = Math.ceil(Math.random() * 9);
+  let machineSelect = '';
+  if (randomNumber <= 3) {
+    machineSelect = 'rock';
+  } else if (randomNumber >= 7) {
+    machineSelect = 'paper';
+  } else {
+    machineSelect = 'scissors';
+  }
+  console.log(randomNumber);
+
+  console.log(machineSelect);
+
+  /* Compares user vs machine: rock:0-3, paper:7+, else: scissors*/
+  //   if ()
 }
-
-
-
 
 //EVENTS
 btn.addEventListener('click', handleClick);
