1650246b65ed6ffdbb2808d32092ee9ca6ed3ae7
Irene García <irenegwodak@gmail.com>
2023-09-19 09:50:11 +0200

    add js comparative user vs machine & print result

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ console.log('ikuyo! :)');
 
 // DECLARATIONS
 
-const inputSelect = document.querySelector('.js-js-select');
+const inputSelect = document.querySelector('.js-select');
 const btn = document.querySelector('.js-button');
 const printElement = document.querySelector('.js-print-result');
 
@@ -12,10 +12,8 @@ function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-//SPECIFIC FUNCTIONS
+// SPECIFIC FUNCTIONS
 function handleClick() {
-  //   const userSelect = inputSelect.value;
-
   /* machine: rock:0-3, paper:7+, else: scissors*/
   const randomNumber = Math.ceil(Math.random() * 9);
   let machineSelect = '';
@@ -27,11 +25,28 @@ function handleClick() {
     machineSelect = 'scissors';
   }
   console.log(randomNumber);
-
   console.log(machineSelect);
 
   /* Compares user vs machine: rock:0-3, paper:7+, else: scissors*/
-  //   if ()
+  const userSelect = inputSelect.value;
+  //user wins
+  if (userSelect === 'paper' && machineSelect === 'rock') {
+    printElement.innerHTML = '¡Has ganado!';
+  }
+  else if (userSelect === 'rock' && machineSelect === 'scissors') {
+    printElement.innerHTML = '¡Has ganado!';
+  }
+  else if (userSelect === 'scissors' && machineSelect === 'paper') {
+    printElement.innerHTML = '¡Has ganado!';
+  } 
+  // draw
+  else if (userSelect === machineSelect) {
+    printElement.innerHTML = 'Empate';
+  }
+  else {
+    printElement.innerHTML = '¡Has perdido!';
+  }
+//   console.log(userSelect);
 }
 
 //EVENTS
