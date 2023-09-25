1a6ab81c1bf8c446ac1ff0658ddcc5c76cdb354f
Irene García <irenegwodak@gmail.com>
2023-09-25 03:36:46 +0200

    add machine option msg + draw counter + result list + end game cat gifs + show/hide track function + show/hide containers improving ux + mediaqueries

--- js/main.js
+++ js/main.js
@@ -1,35 +1,66 @@
 'use strict';
 
+// HTML ELEMENT FUNCTIONS
+//Make easier call html elements
+const getElement = (element) => document.querySelector(element);
+//Make easier write innnerHTML elements
+const trackElement = (element, msg) => (element.innerHTML = msg);
+//Make easier write classList
+const addStyle = (element, classElement) => element.classList.add(classElement);
+const removeStyle = (element, classElement) =>
+  element.classList.remove(classElement);
+const hideElement = (element) => addStyle(element, 'hidden');
+const showElement = (element) => removeStyle(element, 'hidden');
+
 // DECLARATIONS
 const inputSelect = getElement('.js-select');
 const btnPlay = getElement('.js-play-button');
 const endGameHtmlBlock = getElement('.js-endgame-container');
+const endGameImg = getElement('.js-engdame-img');
 const btnReset = getElement('.js-reset-button');
 
+//ICONS
+const iconRock = '&#9994;';
+const iconPaper = '&#128400;&#65039;';
+const iconScissors = '&#9996;&#65039;';
+const iconDraw = '&#128566;';
+
 //PRINTERS
-const printElement = getElement('.js-print');
+const mainPrinterContainer = getElement('.js-main-printer-container');
+const printerMainElement = getElement('.js-print');
+const printMachineOpt = getElement('.js-print-machine-option');
 const printEndGame = getElement('.js-endgame');
 const totalAttemptsPerGame = getElement('.js-attempts');
+const playedGames = getElement('.js-played-games-list');
 
 //COUNTERS
 const counterUser = getElement('.js-counter-user');
 const counterMachine = getElement('.js-counter-machine');
+const counterDraw = getElement('.js-draws');
 const counterGame = getElement('.js-counter-game');
 
 let userCount = 0;
 let machineCount = 0;
+let drawCount = 0;
 let gameCount = 0;
+
+///////// CHOOSE ATTEMPTS /////////
+const defaultAttemptsPerGame = 10;
 
 //MESSAGES TO PRINT
 //Default
-const defaultMsg = 'Dale a <span class="play-button"> Jugar</span> para empezar';
-const defaultAttemptsPerGame = 10;
-trackElement(printElement, defaultMsg);
+const defaultMsg =
+  'Dale a <span class="play-button"> Jugar</span> para empezar';
+trackElement(printerMainElement, defaultMsg);
 trackElement(totalAttemptsPerGame, defaultAttemptsPerGame);
 
 const warning = '¡Escoge piedra, papel o tijera!';
 
-//Messages during the game
+//Messages of computer option
+const machineRock = `La máquina ha escogido piedra ${iconRock}`;
+const machinePaper = `La máquina ha escogido papel ${iconPaper}`;
+const machineScissors = `La máquina ha escogido tijeras ${iconScissors}`;
+//Messages per attempt
 const youWin = 'Has ganado esta jugada &#128588;';
 const youLose = 'Has perdido esta jugada &#128581;';
 const draw = 'Empate &#128566;';
@@ -38,47 +69,48 @@ const msgEndGameWin = '¡Has ganado la partida! &#127881;';
 const msgEndGameDraw = 'Menuda partida, ¡habéis empatado!&#129784;&#129783;';
 const msgEndGameLose = 'Has perdido la partida &#129394;';
 
-
 //FUNCTIONS
-
-// HTML ELEMENT FUNCIONTS
-//To make easier call html elements
-function getElement(element) {
-  return document.querySelector(element);
-}
-//To make easier print innnerHTML elements
-function trackElement(element, msg) {
-  return (element.innerHTML = msg);
-}
 
 // GENERIC FUNCTIONS
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-//Prints on html on the principal advices
+//Prints on html in the principal printer
 function printMsg(msg) {
-  trackElement(printElement, msg);
+  trackElement(printerMainElement, msg);
 }
 
 // SPECIFIC FUNCTIONS
+
+//COUNTERS
 function updateCounters() {
   trackElement(counterUser, userCount);
   trackElement(counterMachine, machineCount);
+  trackElement(counterDraw, drawCount);
   trackElement(counterGame, gameCount);
 }
-//Check and update the counters if iser win, lose or draw
+//Check and update the counters if user win, lose or draw
 function checkCounters(msg) {
   gameCount += 1;
   if (msg !== warning) {
     if (msg === youWin) {
       userCount = userCount + 1;
       updateCounters();
-    }
-    if (msg === youLose) {
+    } else if (msg === youLose) {
       machineCount += 1;
       updateCounters();
-    } else updateCounters();
+    } else {
+      drawCount += 1;
+    }
+    updateCounters();
   }
+}
+
+//MACHINE
+//Let show to user machine selection
+function showMachineOption(msg) {
+  showElement(printMachineOpt);
+  trackElement(printMachineOpt, msg);
 }
 
 //Get random machine option
@@ -89,14 +121,75 @@ function getMachineSelect() {
   let machineSelect = '';
   if (randomNumber <= 3) {
     machineSelect = 'rock';
+    showMachineOption(machineRock);
   } else if (randomNumber >= 7) {
     machineSelect = 'paper';
+    showMachineOption(machinePaper);
   } else {
     machineSelect = 'scissors';
+    showMachineOption(machineScissors);
   }
   return machineSelect;
 }
 
+//PLAYED GAMES LIST CONTAINER
+//Selections in emojis format
+function userOptIcon(userOpt) {
+  let userEmoji = '';
+  switch (userOpt) {
+    case 'rock':
+      userEmoji = iconRock;
+      break;
+    case 'paper':
+      userEmoji = iconPaper;
+      break;
+    default:
+      userEmoji = iconScissors;
+  }
+  return userEmoji;
+}
+function machineOptIcon(machineOpt) {
+  let machineEmoji = '';
+  switch (machineOpt) {
+    case 'rock':
+      machineEmoji = iconRock;
+      break;
+    case 'paper':
+      machineEmoji = iconPaper;
+      break;
+    default:
+      machineEmoji = iconScissors;
+  }
+  return machineEmoji;
+}
+let playedGamesList = '';
+function printWhoWins(result, userEmoji, machineEmoji) {
+  let whoWins = '';
+  switch (result) {
+    case youWin:
+      whoWins = userEmoji;
+      playedGamesList += `<li>${gameCount}. ${userEmoji} VS ${machineEmoji} Gana ${whoWins}</li>`;
+      break;
+    case youLose:
+      whoWins = machineEmoji;
+      playedGamesList += `<li>${gameCount}. ${userEmoji} VS ${machineEmoji} Gana ${whoWins}</li>`;
+      break;
+    default:
+      whoWins = iconDraw;
+      playedGamesList += `<li>${gameCount}. ${userEmoji} VS ${machineEmoji} Empate ${whoWins}</li>`;
+  }
+  trackElement(playedGames, playedGamesList);
+}
+
+//Print the result games in the "Played Games" list container with emojis
+function printPlayedGame(userOpt, machineOpt, result) {
+  let userEmoji = userOptIcon(userOpt);
+  let machineEmoji = machineOptIcon(machineOpt);
+  printWhoWins(result, userEmoji, machineEmoji);
+}
+
+//MAIN GAME
+//Get the results and print them
 function userVsMachine(userValue) {
   let msg = '';
   const machineSelect = getMachineSelect();
@@ -118,30 +211,41 @@ function userVsMachine(userValue) {
   }
   printMsg(msg);
   checkCounters(msg);
+  printPlayedGame(userValue, machineSelect, msg);
 }
+
+//START
 function playGame() {
   //Check if user selected an option
   const userSelect = inputSelect.value;
   if (userSelect === 'default') {
     printMsg(warning);
-    printElement.classList.add("red");
-
+    hideElement(printMachineOpt);
+    addStyle(printerMainElement, 'red-warning');
   } else {
     userVsMachine(userSelect);
-    printElement.classList.remove("red");
+    removeStyle(printerMainElement, 'red-warning');
   }
 }
-//Check if user reached the attempts
+//STOP: Check if user reached the attempts
 function endGame() {
   if (gameCount >= defaultAttemptsPerGame) {
-    btnPlay.classList.add('hidden');
-    endGameHtmlBlock.classList.remove('hidden');
+    hideElement(inputSelect);
+    hideElement(btnPlay);
+    hideElement(mainPrinterContainer);
+    showElement(endGameHtmlBlock);
     if (userCount > machineCount) {
       trackElement(printEndGame, msgEndGameWin);
+      endGameImg.src = 'https://media.tenor.com/06026DSpi60AAAAd/happy-cat.gif';
+      endGameImg.alt = 'Happy Cat';
     } else if (userCount === machineCount) {
       trackElement(printEndGame, msgEndGameDraw);
+      endGameImg.src = 'https://media.tenor.com/OSVtpDZ9E7kAAAAC/discord.gif';
+      endGameImg.alt = 'Poker Cat';
     } else {
       trackElement(printEndGame, msgEndGameLose);
+      endGameImg.src = 'https://media.tenor.com/uu9seSBtPaEAAAAC/sad-cat.gif';
+      endGameImg.alt = 'Angry Cat';
     }
   }
 }
@@ -149,6 +253,7 @@ function endGame() {
 function resetCounters() {
   userCount = 0;
   machineCount = 0;
+  drawCount = 0;
   gameCount = 0;
   updateCounters();
 }
@@ -165,12 +270,21 @@ function handleClickPlay(event) {
 function handleClickReset(event) {
   event.preventDefault();
 
-  inputSelect.value = 'default';
   resetCounters();
-  trackElement(printElement, defaultMsg);
-  btnPlay.classList.remove('hidden');
+  inputSelect.value = 'default';
+  showElement(inputSelect);
+  showElement(btnPlay);
+  showElement(mainPrinterContainer);
+
+  hideElement(printMachineOpt);
+  hideElement(endGameHtmlBlock);
+  endGameImg.src = '';
+  endGameImg.alt = '';
+
+  trackElement(printerMainElement, defaultMsg);
   trackElement(printEndGame, '');
-  endGameHtmlBlock.classList.add('hidden');
+  playedGamesList = '';
+  trackElement(playedGames, '');
 }
 
 //EVENTS
